41402d41efd70048bfc8b292b1c07266ad8041d9
王昊天 <wanghaotian@sinosoft.com.cn>
2020-04-26 01:12:28 +0800

    试着封装百度地图查询API

--- src/main/java/com/wanghaotian/example/MapSearchTest.java
+++ src/main/java/com/wanghaotian/example/MapSearchTest.java
@@ -1,6 +1,5 @@
 package com.wanghaotian.example;
 
-import com.github.kevinsawicki.http.HttpRequest;
 import com.wanghaotian.example.utils.mapsearch.BaiduMapSearchUtils;
 import com.wanghaotian.example.utils.mapsearch.PlaceBaiduMapSearchObject;
 
@@ -19,12 +18,5 @@ public class MapSearchTest {
         placeBaiduMapSearchObject.setRegion("北京");
         placeBaiduMapSearchObject.setTag("银行");
         BaiduMapSearchUtils.getResult(placeBaiduMapSearchObject,PlaceBaiduMapSearchObject.class);
-//        String responce= HttpRequest.get("http://api.map.baidu.com/place/v2/search?query=ATM机&tag=银行&region=北京&output=json&ak=GQg4czMTKd2csVGqYGCyTkAyxFDu08fL").body();
-//        System.out.println(responce);
-
-
-//        http://api.map.baidu.com/place/v2/search?query=ATM机&tag=银行&region=北京&output=json&ak=gqg4czmtkd2csvgqygcytkayxfdu08fl
-//        http://api.map.baidu.com/place/v2/search?region=北京&citylimit=false&query=atm机&output=json&pagesize=40&pagenum=0&ak=gqg4czmtkd2csvgqygcytkayxfdu08fl
-
     }
 }
